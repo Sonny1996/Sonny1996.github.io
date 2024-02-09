abb74bad9721e5b284a2b3fc8e72ce6ffa268aaf
Sun Sonny <xbyning@gmail.com>
2024-02-10 00:13:37 +0800

    Site updated: 2024-02-10 00:13:35

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "12034",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "ffb8e9a7c4ee4f84a9741e81eac5575a"
+  }
+}
